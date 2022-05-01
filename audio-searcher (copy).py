f22be9e0c513fcaacce96ec8dab0e7ff6340fe7c
Sourcery AI <>
2022-05-01 12:49:13 +0000

    'Refactored by Sourcery'

--- audio-searcher (copy).py
+++ audio-searcher (copy).py
@@ -14,7 +14,7 @@ def get_large_audio_transcription(path):
     and apply speech recognition on each of these chunks
     """
     # open the audio file using pydub
-    sound = AudioSegment.from_wav(path) 
+    sound = AudioSegment.from_wav(path)
     file_name = os.path.basename(path) 
 
     # split audio sound where silence is 700 miliseconds or more and get chunks
@@ -31,7 +31,7 @@ def get_large_audio_transcription(path):
     if not os.path.isdir(folder_name):
         os.mkdir(folder_name)
     whole_text = ""
-    # process each chunk 
+    # process each chunk
     for i, audio_chunk in enumerate(chunks, start=1):
         # export audio chunk and save it in
         # the `folder_name` directory.
@@ -45,39 +45,41 @@ def get_large_audio_transcription(path):
                 text = r.recognize_google(audio_listened)
 
             except sr.UnknownValueError as e:
-                print("Error:", str(e))
+                print("Error:", e)
             else:
                 text = f"{text.capitalize()}. "
                 print(chunk_filename, ":", text)
                 whole_text += text
-                
+
                 os.remove(chunk_filename)
     # return the text for all chunks detected
     return whole_text
 
 def find_keyword(keyword):
-	for f in os.scandir("/home/dwp3410/projects/py-audio"):
-   	    ext = os.path.splitext(f)[-1].lower()
-   	    if f.is_file and ext.endswith(".txt"):
-   	       print("searching in f" + str(f))
-   	       with open(f, 'r') as infile:
-   	           for line in infile:
-   	           	if keyword in line:
-                          print("keyword matched in file " + str(f))
+    for f in os.scandir("/home/dwp3410/projects/py-audio"):
+        ext = os.path.splitext(f)[-1].lower()
+        if f.is_file and ext.endswith(".txt"):
+            print(f"searching in f{str(f)}")
+            with open(f, 'r') as infile:
+                for line in infile:
+                    if keyword in line:
+                        print(f"keyword matched in file {str(f)}")
                
 
 if __name__ == '__main__':
 
-	keyword = input("Enter your search keyword: ")
-	
-	for file_name in os.scandir("/home/dwp3410/projects/py-audio"):
-		ext = os.path.splitext(file_name)[-1].lower()
-		print("filename: " + str(file_name))
-		if file_name.is_file and ext.endswith(".wav") and not os.path.exists(f"{os.path.basename(file_name)}" + ".txt"):
-			fh = open(f"{os.path.basename(file_name)}" + ".txt", "w+")
-			fh.write((get_large_audio_transcription(file_name)))
-			fh.close()
-	
-	find_keyword(keyword)		
+    keyword = input("Enter your search keyword: ")
+
+    for file_name in os.scandir("/home/dwp3410/projects/py-audio"):
+        ext = os.path.splitext(file_name)[-1].lower()
+        print(f"filename: {str(file_name)}")
+        if (
+            file_name.is_file
+            and ext.endswith(".wav")
+            and not os.path.exists(f"{os.path.basename(file_name)}.txt")
+        ):
+            with open(f"{os.path.basename(file_name)}.txt", "w+") as fh:
+                fh.write((get_large_audio_transcription(file_name)))
+    find_keyword(keyword)		
 
                            		
--- audio-searcher.py
+++ audio-searcher.py
@@ -13,7 +13,7 @@ tokenizer = RegexpTokenizer(r'\w+')
 
 def transcribe_audio(path):
 
-    audio = AudioSegment.from_wav(path) 
+    audio = AudioSegment.from_wav(path)
     file_name = os.path.basename(path) 
 
     chunks = split_on_silence(audio,
@@ -24,7 +24,7 @@ def transcribe_audio(path):
     audio_folder = "audio-chunks"
     if not os.path.isdir(audio_folder):
         os.mkdir(audio_folder)
-        
+
     audio_text = ""
 
     for i, audio_chunk in enumerate(chunks, start=1):
@@ -37,35 +37,33 @@ def transcribe_audio(path):
                 text = speech_recognizer.recognize_google(audio_file)
 
             except sr.UnknownValueError as e:
-                print("An error has occurred while recognising audio file:", str(e))
+                print("An error has occurred while recognising audio file:", e)
             else:
                 text = f"{text.capitalize()}. "
                 print(chunk_filename, ":", text)
                 audio_text += text
-                
+
                 os.remove(chunk_filename)
-                
+
     return audio_text
 
 def find_keyword(keyword):
-   	for f in os.scandir("/home/kirshi/py-searchable-audio/texts"):
-   	    print("searching in f" + str(f))
-   	    file_content = open(f).read()
-   	    text_tokens = tokenizer.tokenize(file_content)
-   	    text = Text(text_tokens)
-   	    print(text.concordance(keyword))
+    for f in os.scandir("/home/kirshi/py-searchable-audio/texts"):
+        print(f"searching in f{str(f)}")
+        file_content = open(f).read()
+        text_tokens = tokenizer.tokenize(file_content)
+        text = Text(text_tokens)
+        print(text.concordance(keyword))
  
    	       
 def start_search(keyword):
-	for file_name in os.scandir("/home/kirshi/py-searchable-audio"):
-		extension = os.path.splitext(file_name)[-1].lower()
-		file_path = f"texts/{os.path.basename(file_name).split('.')[0] }" + ".txt"
-		if file_name.is_file and extension.endswith(".wav") and not os.path.exists(file_path):
-			fh = open(file_path, "w+")
-			fh.write(transcribe_audio(file_name))
-			fh.close() 
-			
-	find_keyword(keyword)		              
+    for file_name in os.scandir("/home/kirshi/py-searchable-audio"):
+        extension = os.path.splitext(file_name)[-1].lower()
+        file_path = f"texts/{os.path.basename(file_name).split('.')[0]}.txt"
+        if file_name.is_file and extension.endswith(".wav") and not os.path.exists(file_path):
+            with open(file_path, "w+") as fh:
+                fh.write(transcribe_audio(file_name))
+    find_keyword(keyword)		              
 
 if __name__ == '__main__':
 
